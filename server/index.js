0dc4ae1b714cd1af273cade22933005759361ea0
Tammy <tammycwong@gmail.com>
2021-09-12 19:25:29 -0400

    testing

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes)
+
+const CONNECTION_URL =
+  "mongodb+srv://tammycwong:maconbacon128@cluster0.iseem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+    //   useFindAndModify: false,
+    //   useCreateIndex: false,
+ })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error));
+
+// mongoose.set('useFindAndModify', false);
+// mongoose.Promise = global.Promise;
